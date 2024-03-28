7cfcceabe6cb0ca739b0a8a8138bf7b3ceaa5258
safarilkom7 <43083391+safarilkom7@users.noreply.github.com>
2024-03-28 17:49:42 +0800

    add

--- src/main.php
+++ src/main.php
@@ -2,10 +2,25 @@
 
 namespace Halosafar\Halo;
 
+use Carbon\Carbon;
+
 class Main
 {
-    public function getHello()
+    public function periodePembayaran($start)
     {
-        return "Hello World!";
+        $tanggalAwal = Carbon::createFromFormat('d-m-Y', $start);
+        $bulanSekarang = $tanggalAwal->format('m');
+        $tahunSekarang = $tanggalAwal->format('Y');
+        if ($tanggalAwal->format('d') >= 21) {
+            $tanggalAwal->addMonths(2);
+            $bulanSekarang = $tanggalAwal->format('m');
+            $tahunSekarang = $tanggalAwal->format('Y');
+        } else {
+            $tanggalAwal->addMonths(1);
+            $bulanSekarang = $tanggalAwal->format('m');
+            $tahunSekarang = $tanggalAwal->format('Y');
+        }
+        $tanggalPembayaran = Carbon::create($tahunSekarang, $bulanSekarang, 5, 0, 0, 0);
+        return $tanggalPembayaran->format('d-m-Y');
     }
 }
